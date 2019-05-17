5c7ad7b86ea23a03a9dda6e4d0c9f75e1dffe3ac
Neha Nautiyal <nehahaha@gmail.com>
2019-05-17 15:40:29 -0400

    edited jawsdb configuration

--- config/config.js
+++ config/config.js
@@ -1,10 +1,19 @@
-{
+module.exports = {
+
   "development": {
+<<<<<<< HEAD:config/config.json
     "username": "root",
     "password": "0420",
     "database": "betsdb",
     "host": "127.0.0.1",
     "port": 3306,
+=======
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.PORT || 3306,
+>>>>>>> edited jawsdb configuration:config/config.js
     "dialect": "mysql"
   },
   "test": {
@@ -16,11 +25,15 @@
     "dialect": "mysql"
   },
   "production": {
+<<<<<<< HEAD:config/config.json
     "username": "root",
     "password": "0420",
     "database": "database_production",
     "host": "127.0.0.1",
     "port": 3306,
+=======
+    "use_env_variable": "JAWSDB_URL",
+>>>>>>> edited jawsdb configuration:config/config.js
     "dialect": "mysql"
   }
 }
