7956e42818de908e7a56a24cebde2aaa7df1216b
Esin Gokgoz <esingokgoz@gmail.com>
2019-05-18 14:04:13 -0400

    created views folder for html

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
 
   "development": {
-    "username": process.env.MYSQL_USER || "root",
-    "password": process.env.MYSQL_KEY || "" || "root",
-    "database": process.env.MYSQL_DBNAME || "betsdb",
-    "host": process.env.MYSQL_HOST || "127.0.0.1",
+    "username": "root",
+    "password": "root",
+    "database": "betsdb",
+    "host": "127.0.0.1",
     "port": process.env.PORT || 3306 || 8889,
     "dialect": "mysql"
   },
