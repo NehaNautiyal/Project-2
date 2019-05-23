f864c7bdf39752207700496e1ee0e90c483170ca
wabbot <wabbot24@gmail.com>
2019-05-23 16:04:54 -0400

    -.put method for changing balance when user makes a bet

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,18 +1,18 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  app.get("/api/users", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/users", function (req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
     db.User.findAll({
       include: [db.Bet]
-    }).then(function(dbAuthor) {
+    }).then(function (dbAuthor) {
       res.json(dbAuthor);
     });
   });
 
-  app.get("/api/users/:id", function(req, res) {
+  app.get("/api/users/:id", function (req, res) {
     // Here we add an "include" property to our options in our findOne query
     // We set the value to an array of the models we want to include in a left outer join
     // In this case, just db.Post
@@ -21,23 +21,32 @@ module.exports = function(app) {
         id: req.params.id
       },
       include: [db.Bet]
-    }).then(function(dbAuthor) {
+    }).then(function (dbAuthor) {
       res.json(dbAuthor);
     });
   });
 
-  app.post("/api/users", function(req, res) {
-    db.User.create(req.body).then(function(dbAuthor) {
+  app.post("/api/users", function (req, res) {
+    db.User.create(req.body).then(function (dbAuthor) {
       res.json(dbAuthor);
     });
   });
 
-  app.delete("/api/users/:id", function(req, res) {
+  app.put("/api/users/:id", function (req, res) {
+    console.log(req.body.data);
+    console.log(req.params);
+    db.User.update(
+      { balance: req.body.data},
+      { where: {id: req.params.id} }
+    ).then(() => { });
+  });
+
+  app.delete("/api/users/:id", function (req, res) {
     db.User.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAuthor) {
+    }).then(function (dbAuthor) {
       res.json(dbAuthor);
     });
   });
