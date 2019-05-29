aaf2afdc780fae8a48b392b9686d03ce36b4d45a
Neha Nautiyal <nehahaha@gmail.com>
2019-05-29 19:05:41 -0400

    drop database

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ require("./routes/email-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({force: false}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
