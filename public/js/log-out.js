a1c6e2ae894ae92becdbbabeb24866a58f9626e5
ann-leonard <aleo0779@gmail.com>
2019-05-30 14:08:25 -0400

    fixed nav link

--- public/js/log-out.js
+++ public/js/log-out.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
+let logOut = $("#logOut")
+let dropdown = $("#dropdown")
+let accountBtn = $("#accBtn")
+
+dropdown.attr("style", "display:none")
+
 $.get("/isLogged", function(data){
     if (data.logOutBtn){
     console.log("log")
-    $("#logOut").attr("style", "display:block")    
+    logOut.attr("style", "display:block") 
+    accountBtn.addClass("dropdown-toggle")
+    dropdown.attr("style", "display:block")
+ 
     }else{
         console.log("not log")
     }
