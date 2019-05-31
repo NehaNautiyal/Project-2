08239e70b58d5a6e1b95b86aea693f6476b175b3
ann-leonard <aleo0779@gmail.com>
2019-05-30 20:30:01 -0400

    validation for new users

--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,25 +1,13 @@
-function insertUser(userData) {
-    $.post("/api/users", userData)
-      .then(getUser);
-  }
 
-function getUser() {
+$("#submitLogIn").on('click', function(){
+    event.preventDefault()
+    let username = $('#usernameLog').val().trim();
+    let password = JSON.parse($('#passLog').val().trim());
     $.get('/api/users',function(data){
         console.log(data)
         console.log(username)
         console.log(password)
-    }).then(function(){
+    }).then(function(data){
         window.location.replace("/")
-    }).catch(function(err) {
-        console.log(err);
-    });
-}
-
-$("#submitLogIn").on('click', function(){
-    event.preventDefault()
-    let name = $('#usernameLog').val().trim();
-    let email = $('#passInput').val().trim();
-    let password = JSON.parse($('#passLog').val().trim());
-
-    insertUser({name, email, password});
-});
+    })
+})
